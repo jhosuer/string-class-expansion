eccc562bcedd0c65e57c922b9b9a6f177d83322d
Jhosuer <jumbojoshua91@gmail.com>
2018-12-19 16:34:09 +0100

    added the to upper method

--- String Methods/String.js
+++ String Methods/String.js
@@ -5,4 +5,21 @@ String.prototype.hasVowels = function() {
     return checker.test(str);
   }
 
+  String.prototype.toUpper = function(){
+    let str = Object.values(this).join('');
+    let strNew = str.split('');
+    let strN = [];
+    for(let index = 0; index < strNew.length; index++) {
+      if((str[index].charCodeAt() >= 97) && (str[index].charCodeAt() <= 121)) {
+        let a = str[index].charCodeAt() - 32;
+      strN.push(String.fromCharCode(a));  
+      }
+      else{
+        strN.push(str[index]);
+      }
+    }
+    let nn = strN.join('');
+    return nn;
+  }
+
 module.exports = String;
